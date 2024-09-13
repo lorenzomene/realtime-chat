841fdf580a14e7cb55af085c436caa36bd39f73a
lorenzomene <lorenzospmene@gmail.com>
2024-09-13 17:52:19 -0300

    feat: add basic rachet usage for connection/dc/message

--- src/Chat.php
+++ src/Chat.php
@@ -8,11 +8,50 @@ use Ratchet\ConnectionInterface;
 
 class Chat implements MessageComponentInterface
 {
-    public function onOpen(ConnectionInterface $conn) {}
+    protected $clients;
 
-    public function onMessage(ConnectionInterface $from, $msg) {}
+    public function __construct()
+    {
+        $this->clients = new \SplObjectStorage;
+    }
 
-    public function onClose(ConnectionInterface $conn) {}
+    public function onOpen(ConnectionInterface $conn)
+    {
+        $this->clients->attach($conn);
 
-    public function onError(ConnectionInterface $conn, Exception $e) {}
+        echo "New connection ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+        $connectionsNumber = count($this->clients) - 1;
+
+        echo sprintf(
+            'Connection %d sent message "%s" to %d other connections%s' . "\n",
+            $from->resourceId,
+            $msg,
+            $connectionsNumber,
+            $connectionsNumber == 1 ? '' : 's'
+        );
+
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                $client->send($msg);
+            }
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn)
+    {
+        $this->clients->detach($conn);
+
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+
+    public function onError(ConnectionInterface $conn, Exception $e)
+    {
+        echo "An error has occured: {$e->getMessage()}\n";
+
+        $conn->close();
+    }
 }
