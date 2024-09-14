35041556f3e98384769cabb699064533a1232e2a
lorenzomene <lorenzospmene@gmail.com>
2024-09-14 16:44:10 -0300

    feat: add usernames and country to connections(users)

--- src/Chat.php
+++ src/Chat.php
@@ -9,6 +9,7 @@ use Ratchet\ConnectionInterface;
 class Chat implements MessageComponentInterface
 {
     protected $clients;
+    protected int $activeUserCount;
 
     public function __construct()
     {
@@ -20,25 +21,24 @@ class Chat implements MessageComponentInterface
         $this->clients->attach($conn);
 
         echo "New connection ({$conn->resourceId})\n";
+
+        $conn->user = null;
+        $conn->country = null;
+        $conn->personalConfig = null;
     }
 
     public function onMessage(ConnectionInterface $from, $msg)
     {
-        $connectionsNumber = count($this->clients) - 1;
-
-        echo sprintf(
-            'Connection %d sent message "%s" to %d other connections%s' . "\n",
-            $from->resourceId,
-            $msg,
-            $connectionsNumber,
-            $connectionsNumber == 1 ? '' : 's'
-        );
-
-        foreach ($this->clients as $client) {
-            if ($from !== $client) {
-                $client->send($msg);
+        if ($from->user === null) {
+            $setup = $this->processUserSetup($from, $msg);
+            if (!$setup) {
+                $from->send(
+                    "Follow the error instructions for setting up your connection"
+                );
             }
         }
+
+        $success = $this->processMessage($from, $msg);
     }
 
     public function onClose(ConnectionInterface $conn)
@@ -53,5 +53,38 @@ class Chat implements MessageComponentInterface
         echo "An error has occured: {$e->getMessage()}\n";
 
         $conn->close();
+    }
+
+    private function processUserSetup(ConnectionInterface $conn, $msg): bool
+    {
+        $data = explode('|', $msg);
+
+        if (count($data) !== 2) {
+            $conn->send(
+                "Error: your first message must be your user and country in the format: user|country"
+            );
+            return false;
+        }
+
+        $conn->user = trim($data[0]);
+        $conn->country = trim($data[1]);
+
+        $conn->send("Welcome {$conn->user} from {$conn->country}!\n");
+        return true;
+        //TODO: broadcast function between clients
+    }
+
+    private function processMessage(ConnectionInterface $conn, $msg): bool
+    {
+        $formattedMessage = "{$conn->user}({$conn->country}): {$msg}\n";
+        //TODO: broadcast
+
+        echo "Message from {$conn->user} sent";
+        return true;
+    }
+
+    private function broadcastMessage()
+    {
+        $connectionsNumber = count($this->clients) - 1;
     }
 }
